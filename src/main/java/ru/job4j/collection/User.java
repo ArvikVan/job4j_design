8ec20989578b1ed2ecad73c45fa7c1b1fbbb4c63
ArvikVan <arvikvan@gmail.com>
2021-05-23 23:56:43 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -2,10 +2,14 @@ package ru.job4j.collection;
 /**
  * класс описывает модель данных
  * @author arvik
- * @version 1.0
+ * @version 1.1
+ *  Без переопределения equals и hashCode
  */
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -16,5 +20,28 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        User first = new User("FirstUser", 1,
+                new GregorianCalendar(1981, 05, 31));
+        User second = new User("SecondUser", 1,
+                new GregorianCalendar(1981, 05, 31));
+        Map<User, Object> userObjectMap = new HashMap<>();
+        userObjectMap.put(first, new Object());
+        userObjectMap.put(second, new Object());
+        for (Map.Entry<User, Object> user : userObjectMap.entrySet()) {
+            System.out.println("key: " + user.getKey());
+            System.out.println("value: " + user.getValue());
+        }
     }
 }
