d3993b518c9b3715d842fbab547ba85c3efddfe6
ArvikVan <arvikvan@gmail.com>
2021-06-08 20:45:26 +0300

    1. Экзамен. Структуры данных и алгоритмы. [#1000 #164644]

--- src/main/java/ru/job4j/exam/MergeUsers.java
+++ src/main/java/ru/job4j/exam/MergeUsers.java
@@ -1,2 +1,49 @@
-package ru.job4j.exam;public class MergeUsers {
+package ru.job4j.exam;
+/**
+ * кдасс описывает слияние пользователей с их мылами
+ * @author arvik
+ * @version 1.0
+ */
+
+import java.util.*;
+
+public class MergeUsers {
+    /**
+     *
+     * @param mergeOfUsers на входе карта пользователей с их емайлами
+     * @return на выходе результат слияния пользователей
+     * со0здаем карту in и добавляем в нее значения mergeOfUsers
+     * создаем карту out в которой будет результат слияния
+     * бежим по in и заполняем out
+     * добавляем значения по ключу(имя) и значению(множество почт)
+     *
+     * бежим по out и добавляем в set все элементы из inElement и outElement которых нет
+     * если сумма уникальных почт и сумма почт inElement и outElement не равны, то
+     * in и out равны
+     * записываем уникальные почты в in, ключ в out
+     * добавляем в щге ключ, значение
+     * возвращаем результат слияния
+     */
+    public static Map<String, Set<String>> merge(Map<String, Set<String>> mergeOfUsers) {
+        Map<String, Set<String>> in = new HashMap<>(mergeOfUsers);
+        Map<String, Set<String>> out = new HashMap<>();
+        for (Map.Entry<String, Set<String>> inElement: in.entrySet()) {
+                if (out.isEmpty()) {
+                out.put(inElement.getKey(), inElement.getValue());
+                continue;
+            }
+            for (Map.Entry<String, Set<String>> outElement: out.entrySet()) {
+                Set<String> set = new HashSet<>();
+                set.addAll(inElement.getValue());
+                set.addAll(outElement.getValue());
+                int fullSize = outElement.getValue().size()
+                        + inElement.getValue().size();
+                if (fullSize != set.size()) {
+                    inElement.setValue(set);
+                }
+                out.put(outElement.getKey(), inElement.getValue());
+            }
+        }
+        return out;
+    }
 }
--- src/test/java/ru/job4j/exam/MergeUsersTest.java
+++ src/test/java/ru/job4j/exam/MergeUsersTest.java
@@ -1,6 +1,29 @@
 package ru.job4j.exam;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
 public class MergeUsersTest {
-  
+    @Test
+    public void whenEmailEqualThenMerge() {
+        Map<String, Set<String>> input = new HashMap<>();
+        input.put("user1", Set.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru"));
+        input.put("user2", Set.of("foo@gmail.com", "ups@pisem.net"));
+        Map<String, Set<String>> out = new HashMap<>();
+        out.put("user1", Set.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru", "ups@pisem.net"));
+        assertThat(MergeUsers.merge(input), is(out));
+    }
+
+    @Test
+    public void whenNoKey() {
+        Map<String, Set<String>> input = new HashMap<>();
+        input.put("user1", Set.of("foo@gmail.com", "hoo@gmail.com", "zoo@gmail.com"));
+        input.put("user2", Set.of("foo@gmail.com", "hoo@gmail.com", "zoo@gmail.com"));
+        Map<String, Set<String>> out = MergeUsers.merge(input);
+        assertThat(out.get("user2"), nullValue());
+    }
 }
