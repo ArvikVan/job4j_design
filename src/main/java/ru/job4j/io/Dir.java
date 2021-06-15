d37c576daab2d841f6261dc01846b3037a96fe30
ArvikVan <arvikvan@gmail.com>
2021-06-15 22:10:40 +0300

    without String.format

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -2,22 +2,22 @@ package ru.job4j.io;
 /**
  * класс описывает работу с директориями
  * @author arvik
- * @version 1.0
+ * @version 1.1
  * file.getName(), file.length(), file.getTotalSpace()
+ * без String.format
  */
 
 import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-            File file = new File("/home/arv/IdeaProjects");
+            File file = new File("/home/arvik/IdeaProjects");
             if (!file.exists()) {
                 throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
             }
             if (!file.isDirectory()) {
                 throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
             }
-            System.out.println(String.format
-                    ("The fileName: %s, and the fileLength: %s", file.getName(), file.length()));
+            System.out.printf("The fileName: %s, and the fileLength: %s%n", file.getName(), file.length());
     }
 }
