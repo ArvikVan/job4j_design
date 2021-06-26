83dcfc5c19781041d2ae46e1f18184861e2c6a2c
ArvikVan <arvikvan@gmail.com>
2021-06-27 00:20:17 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -2,9 +2,11 @@ package ru.job4j.io;
 /**
  * класс описывает логгирование системы
  * @author arvik
- * @version 1.1
+ * @version 1.2
  * заменены импорты и подключена в поме библиотека slf4j
  * private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+ * Создайте 8 переменных с примитивным типом. Все типы должны быть разными.
+ * Выведите переменные на консоль через логгер.
  */
 
 import org.apache.log4j.BasicConfigurator;
@@ -16,10 +18,17 @@ public class UsageLog4j {
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int i = Integer.MAX_VALUE;
+        byte b = Byte.MAX_VALUE;
+        long l = Long.MAX_VALUE;
+        short s = Short.MAX_VALUE;
+        char c = 1010;
+        float f = Float.MAX_VALUE;
+        double d = Double.MAX_VALUE;
+        boolean bl = true;
+        LOG.debug("\n" + "Value of int: {}, Value of byte: {}, Value of long: {}, "
+                +  "Value of short: {}, Value of char: {}, Value of float: {}, "
+                + "Value of double: {}, Value of boolean: {},",
+                i + "\n", b + "\n", l + "\n", s + "\n", c + "\n", f + "\n", d + "\n", bl);
     }
 }
