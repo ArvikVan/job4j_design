a1fbf365b0c5c057d9c5632815ec1862a78d0a37
ArvikVan <arvikvan@gmail.com>
2021-06-26 23:54:03 +0300

    2. Simple Loggin Facade 4 Java. [#268849]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -2,15 +2,17 @@ package ru.job4j.io;
 /**
  * класс описывает логгирование системы
  * @author arvik
- * @version 1.0
+ * @version 1.1
+ * заменены импорты и подключена в поме библиотека slf4j
+ * private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
  */
 
 import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
