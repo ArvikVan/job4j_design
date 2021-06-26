19b937099987f2aa277687f29f8c3780509cddfe
ArvikVan <arvikvan@gmail.com>
2021-06-26 23:43:42 +0300

    1. Log4j. Логирование системы. [#95335]

---
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+/**
+ * класс описывает логгирование системы
+ * @author arvik
+ * @version 1.0
+ */
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class UsageLog4j {
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+}
