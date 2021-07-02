124524b02673869595c37b09d5d1899722cac259
ArvikVan <arvikvan@gmail.com>
2021-07-03 00:04:41 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/ChatMembers.java
+++ src/main/java/ru/job4j/serialization/json/ChatMembers.java
@@ -3,11 +3,17 @@ package ru.job4j.serialization.json;
  * класс описывает Преобразование объекта в JSON и обратно
  * @author arvik
  * @version 1.0
+ * дабвлены геттеры
+ * изменен main метод преобразованием POJO объекта в JSONObject  и json-строку.
  */
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ChatMembers {
     private final boolean online;
@@ -15,6 +21,26 @@ public class ChatMembers {
     private final String chatName;
     private final Contact contact;
     private final String[] members;
+
+    public boolean isOnline() {
+        return online;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getChatName() {
+        return chatName;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public String[] getMembers() {
+        return members;
+    }
 
     public ChatMembers(boolean online, int age, String chatName, Contact contact, String... members) {
         this.online = online;
@@ -36,13 +62,24 @@ public class ChatMembers {
     }
 
     public static void main(String[] args) {
+
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+        List<String> list = new ArrayList<>();
+        list.add("AlexTer");
+        list.add("Igor Kovalkov");
+        JSONArray jsonMembers = new JSONArray(list);
+
         final ChatMembers chatMembers = new ChatMembers(true,
                 25, "junior", new Contact("11111"), "AlexTer", "Igor Kovalkov");
-        /* in JSON*/
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(chatMembers));
-        /*in object*/
-        final ChatMembers chatMembers1 = gson.fromJson(gson.toJson(chatMembers), ChatMembers.class);
-        System.out.println(chatMembers1);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("online", chatMembers.isOnline());
+        jsonObject.put("age", chatMembers.getAge());
+        jsonObject.put("chatName", chatMembers.getChatName());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("members", jsonMembers);
+
+        System.out.println(jsonObject.toString());
+
+        System.out.println(new JSONObject(chatMembers).toString());
     }
 }
