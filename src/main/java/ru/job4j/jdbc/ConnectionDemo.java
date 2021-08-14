902d3e9639b29a2e45780e87c7f1f954acb2156a
ArvikVan <arvikvan@gmail.com>
2021-08-14 18:48:02 +0300

    0. JDBC [#6863]00

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,32 @@
+package ru.job4j.jdbc;
+/**
+ * @author ArvikV
+ * @version 1.0
+ * указать путь до файла Config config = new Config("./data/app.properties");
+ * урл из файла пропертис String url = config.value("hibernate.connection.url");
+ * логин из файла пропертис String login = config.value("hibernate.connection.username");
+ * пароль из файла пропертис String password = config.value("hibernate.connection.password");
+ * */
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Config config = new Config("./data/app.properties");
+        config.load();
+        String url = config.value("hibernate.connection.url");
+        String login = config.value("hibernate.connection.username");
+        String password = config.value("hibernate.connection.password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
